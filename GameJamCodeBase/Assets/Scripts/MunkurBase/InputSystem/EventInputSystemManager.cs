0c30008d703bf982c05b607801898bd21caf1423
mfkucuk <mfkucuk555@gmail.com>
2023-06-16 19:30:57 +0300

    Basic event input system

---
+++ GameJamCodeBase/Assets/Scripts/MunkurBase/InputSystem/EventInputSystemManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EventInputSystemManager : SingletonnPersistent<EventInputSystemManager>
+{    
+    [SerializeField] private bool _isDebugEnabled;
+    [SerializeField] private List<string> _inputManagerAxesList;
+    private Dictionary<string, Action> _inputManagerAxesDict;
+
+    private void Start() 
+    {
+        _inputManagerAxesDict = new Dictionary<string, Action>();
+
+        foreach (string eventName in _inputManagerAxesList) 
+        {
+            _inputManagerAxesDict.Add(eventName, () => {});
+        }
+    }
+
+    private void Update()
+    {
+        foreach (string eventName in _inputManagerAxesList) 
+        {
+            if (Input.GetButtonDown(eventName)) 
+            {
+                _inputManagerAxesDict[eventName]?.Invoke();
+
+                if (_isDebugEnabled) 
+                {
+                    Debug.Log(eventName);
+                }
+            }
+        }
+    }
+}
